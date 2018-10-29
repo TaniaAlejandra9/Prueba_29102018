210a909b6f7ad6097666ab3c11db89673ebefb0b
LAB2-PC <LAB2-PC@LAB2>
2018-10-29 10:47:15 -0500

    Prueba 29/10/2018 comosubir un archivo

---
+++ src/prueba29_10_2018/HolaMundo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba29_10_2018;
+
+/**
+ *
+ * @author LAB2-PC
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundos");
+    }
+    
+}
